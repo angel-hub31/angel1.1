50cbf6dc2238b4eeac306ed5db659c4a290a4fb4
angel-hub31 <moralesmales1@hotmail.com>
2026-03-26 18:19:09 -0500

    feet: create agregarProducto

--- casa.java
+++ casa.java
@@ -10,5 +10,18 @@ public class casa {
         this.productos = new String[10];
         this.precios = new double[10];
         this.cantidad = 0;
+
+
+        
+    }
+     public void agregarProducto(String nombre, double precio) {
+        if (cantidad < 10) {
+            productos[cantidad] = nombre;
+            precios[cantidad] = precio;
+            cantidad++;
+            System.out.println("Producto agregado: " + nombre + " ($" + precio + ")");
+        } else {
+            System.out.println("El carrito está lleno.");
+        }
     }
 }
